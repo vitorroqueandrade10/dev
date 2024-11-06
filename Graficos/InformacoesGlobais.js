2807b8447be5c958c672df59a6f7c446d46f27b3
vitorroqueandrade10 <130611483+vitorroqueandrade10@users.noreply.github.com>
2024-11-06 20:04:32 -0300

    06/11/2024

---
+++ Graficos/InformacoesGlobais.js
@@ -0,0 +1,20 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas)*100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
+    
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    
+    const container = document.getElementById(graficos-container)
+    container.appendChild(paragrafo)
+  }
+  
+  vizualizarInformacoesGlobais()
